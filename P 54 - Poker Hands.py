89dac69af0b324b44be8acd525e8fb95c6f83394
douglas domingues <scalioni.domingues@gmail.com>
2020-05-26 23:35:22 -0300

    p54 not solved

--- P 54 - Poker Hands.py
+++ P 54 - Poker Hands.py
@@ -1,7 +1,5 @@
-from collections import OrderedDict
-
-file = open('poker.txt', 'r')
-test_line = "TS QH 6C 8H TH 5H 3C 3H 9C 9D"
+file = open('poker.txt', 'r').read()
+lines = file.split('\n')
 
 suits_value = {
     'C': 1,
@@ -40,14 +38,14 @@ def highest_card(hand):
     sum_cards = 0
     for i in range(len(values)):
         sum_cards += values[i]/n**(i+1)
-    return sum_cards, 0
+    return True, sum_cards
 
 
 def one_pair(hand):
     cards = n_cards(hand)
     for c in cards:
         if c[1] == 2:
-            return True, c[0] + highest_card(hand)[0]
+            return True, c[0] + highest_card(hand)[1]
     return False, 0
 
 
@@ -66,16 +64,17 @@ def three_of_a_kind(hand):
     cards = n_cards(hand)
     for c in cards:
         if c[1] == 3:
-            return True, c[0] + highest_card(hand)[0]  # game, tiebreaker
+            return True, c[0] + highest_card(hand)[1]  # game, tiebreaker
     return False, 0
 
 
 def straight(hand):
     values = get_values(hand)
+    values.sort(reverse=False)
     for i in range(0, 4):
         if values[i] != (values[i + 1] - 1):
             return False, 0
-    return True, highest_card(hand)[0]
+    return True, highest_card(hand)[1]
 
 
 def flush(hand):
@@ -84,7 +83,7 @@ def flush(hand):
     for card in hand:
         if card[1] != suit:
             return False, 0
-    return True, highest_card(hand)[0]
+    return True, highest_card(hand)[1]
 
 
 def full_house(hand):
@@ -97,12 +96,15 @@ def four_of_a_kind(hand):
     cards = n_cards(hand)
     for c in cards:
         if c[1] == 4:
-            return True, c[0]  # game, tiebreaker
+            return True, c[0]+highest_card(hand)[1]  # game, tiebreaker
     return False, 0
 
 
 def straight_flush(hand):
-    return straight(hand) and flush(hand), highest_card(hand)[0]  # game, tiebreaker
+    st = straight(hand)[0]
+    fl = flush(hand)[0]
+    tiebreaker = highest_card(hand)[1]
+    return st and fl, tiebreaker  # game, tiebreaker
 
 
 def royal_flush(hand):
@@ -114,7 +116,7 @@ def royal_flush(hand):
     elif 'T' + suit not in hand:
         return False, 0
     else:
-        return True, 0  # game, tiebreaker
+        return True, suits_value[hand[0][1]]/4  # game, tiebreaker
 
 
 def get_values(hand):
@@ -140,10 +142,46 @@ def n_cards(hand):
     return cards
 
 
+def evaluate(hand):
+    r = {
+        0: royal_flush(hand),
+        1: straight_flush(hand),
+        2: four_of_a_kind(hand),
+        3: full_house(hand),
+        4: flush(hand),
+        5: straight(hand),
+        6: three_of_a_kind(hand),
+        7: two_pairs(hand),
+        8: one_pair(hand),
+        9: highest_card(hand)
+    }
+    return r
+
+
 def compare_hands(p1, p2):
-    
+    p1 = evaluate(p1)
+    p2 = evaluate(p2)
+    i = 0
+    while not p1[i][0] and not p2[i][0]:
+        i += 1
+    if p1[i][0] and p2[i][0]:
+        return p1[i][1] > p2[i][1]
+    else:
+        print(p1[i][0], p2[i][0])
+        return p1[i][0] > p2[i][0]
 
 
-a, b = draw(test_line)
-print(b)
-print(two_pairs(b))
+print(len(lines))
+player1_wins = 0
+player2_wins = 0
+for line in lines:
+    player1, player2 = draw(line)
+    print(player1, player2)
+    if compare_hands(player1, player2):
+        print('player1 wins')
+        player1_wins += 1
+    else:
+        player2_wins += 1
+print(player1_wins)
+print(player2_wins)
+
